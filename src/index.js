b2a29e029d7b36b9668e48ceff64e291211690af
Wy <wy@mail.com>
2024-06-20 11:30:39 +0200

    update react version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // Importe tous les membres de la bibliothèque "react" sous le nom "React"
 import * as React from "react";
-// Importe le module "react-dom" depuis la bibliothèque "react-dom"
-import ReactDOM from "react-dom";
 // Importe le composant "App" depuis le fichier "./App.js"
 import App from "./App";
+import { hydrateRoot } from 'react-dom/client';
 
 // Hydrate le composant "App" dans l'élément HTML avec l'identifiant "root"
-ReactDOM.hydrate(<App />, document.getElementById("root"));
+hydrateRoot(document.getElementById("root"), <App />);
